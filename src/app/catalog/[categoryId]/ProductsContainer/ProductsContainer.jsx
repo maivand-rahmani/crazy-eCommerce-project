b4e5b28ae8b42d12409e5877bfdd1b7c8b2b5b21
maivand-rahmani <maivandrahmani@MacBook-Air-Maivand.local>
2026-01-06 13:23:07 +0300

    feat(catalog): filter button added for mobile version

--- src/app/catalog/[categoryId]/ProductsContainer/ProductsContainer.jsx
+++ src/app/catalog/[categoryId]/ProductsContainer/ProductsContainer.jsx
@@ -1,29 +1,35 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import ProductsFilter from './ProductsFilter'
-import ProductsLists from './ProductsLists'
+import { Settings2Icon } from "lucide-react"
+import React, { useState } from "react";
+import ProductsFilter from "./ProductsFilter";
+import ProductsLists from "./ProductsLists";
 import ProductCard from "@/Components/ui/product/ProductCard/ProductCard.jsx";
 
-
 const ProductsContainer = ({ data }) => {
-  let [products , setProducts] = useState(data.data)
+  let [products, setProducts] = useState(data.data);
+  let [open, setOpen] = useState(false);
   
-
   return (
     <div className="flex flex-col md:flex-row p-5 md:p-20 gap-10">
-  {/* Левая панель фильтров */}
-  <div className="md:sticky static top-25 self-start">
-    <ProductsFilter setProducts={setProducts} products={products}   />
-  </div>
+      {/* //кнопка для фильтров */}
+      <div className="md:hidden">
+        <button className=" rounded-2xl flex gap-5 p-3 border shadow-2xl transition-all active:bg-gray-200" onClick={() => setOpen(!open)}>{<Settings2Icon/>} Filters</button>
+      </div>
 
-  {/* Список товаров */}
-  <div className="flex-1">
-    <ProductsLists data={products} />
-  </div>
-</div>
+      {/* Левая панель фильтров */}
+      
+      <div className={`${open ? "static" : "hidden"} md:sticky static top-25 self-start transition-all`} >
+        <ProductsFilter setProducts={setProducts} products={products} />
+      </div>
+      
 
-  )
-}
+      {/* Список товаров */}
+      <div className="flex-1">
+        <ProductsLists data={products} />
+      </div>
+    </div>
+  );
+};
 
-export default ProductsContainer
+export default ProductsContainer;
